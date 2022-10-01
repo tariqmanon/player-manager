9a4000fc673c3a7b6ed551b94976a2a60208e418
tariqmanon <tmanon007@gmail.com>
2022-10-02 04:32:26 +0600

    budget done

---
+++ js/budget.js
@@ -0,0 +1,43 @@
+document.getElementById('player-budget').addEventListener('click', function () {
+    const listItem = document.getElementById('player-list').getElementsByTagName('li');
+    const listCount = listItem.length;
+    const playerField = document.getElementById('player-field');
+    const playerFieldStr = playerField.value;
+    const perPlayerBudget = parseInt(playerFieldStr);
+
+
+
+    if (listCount === 0) {
+        alert("please select a player");
+    } else if (playerFieldStr.length == '') {
+        alert("please Provide player Expense Fee");
+    } else if (perPlayerBudget < 0 && typeof perPlayerBudget !== 'number') {
+        alert("please Provide valid Expense Fee");
+    }
+    else {
+        const getPlayerBudget = perPlayerBudget * listCount;
+        const playerExpense = document.getElementById('player-expense');
+        playerExpense.innerText = getPlayerBudget;
+    };
+
+
+});
+
+document.getElementById('total-expense').addEventListener('click', function () {
+    const managerField = document.getElementById('manager-exp');
+    const managerExp = parseInt(managerField.value);
+    const coachField = document.getElementById('coach-exp');
+    const coachExp = parseInt(coachField.value);
+    const playerField = document.getElementById('player-expense');
+    const playerExp = parseInt(playerField.innerText);
+
+    if (managerExp, coachExp, playerExp != '' && managerExp, coachExp, playerExp > 0) {
+        const totalField = document.getElementById('total-field');
+        const totalExp = managerExp + coachExp + playerExp;
+        totalField.innerText = totalExp;
+    } else {
+        alert('please fill up the field')
+    }
+
+
+})
